55e83d01ecd7555d4201c206747dcf92eb00ee0b
Rodolfo Perez <rodolfobeste@gmail.com>
2019-02-05 22:13:34 +0200

    Created user action components and Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 import Courses from './components/courses/Courses';
 import CourseDetails from "./components/courses/CourseDetails";
+import SignIn from './components/userActions/UserSignIn';
+import UserSignUp from './components/userActions/UserSignUp';
+import CreateCourse from './components/userActions/CreateCourse';
+import UpdateCourse from './components/userActions/UpdateCourse';
+
 
 class App extends Component {
 
   render() {
     return (
       <div>
+        <Header />
         <Courses />
         <CourseDetails />
+        <SignIn />
+        <UserSignUp />
+        <CreateCourse />
+        <UpdateCourse />
       </div>
     );
   }
