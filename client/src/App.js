cebe4fd40d9c3ce435fdbe70fd2c7ab7b53f74ba
Rodolfo Perez <rodolfobeste@gmail.com>
2019-02-06 00:36:56 +0200

    Added app routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import { 
+  BrowserRouter,
+  Route} from "react-router-dom";
 import Header from './components/Header';
 import Courses from './components/courses/Courses';
 import CourseDetails from "./components/courses/CourseDetails";
-import SignIn from './components/userActions/UserSignIn';
+import UserSignIn from './components/userActions/UserSignIn';
 import UserSignUp from './components/userActions/UserSignUp';
+import UserSignOut from "./components/userActions/UserSignOut"
 import CreateCourse from './components/userActions/CreateCourse';
 import UpdateCourse from './components/userActions/UpdateCourse';
 
@@ -13,15 +17,18 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Header />
-        <Courses />
-        <CourseDetails />
-        <SignIn />
-        <UserSignUp />
-        <CreateCourse />
-        <UpdateCourse />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" render={ () => <Courses /> }/>
+          <Route path="/courses/create" render={ () => <CreateCourse /> }/>
+          <Route path="/courses/:id/update" render={ () => <UpdateCourse /> }/>
+          <Route path="/courses/:id" render={ () => <CourseDetails /> }/>
+          <Route path="/signin" render={ () => <UserSignIn /> }/>
+          <Route path="/signup" render={ () => <UserSignUp /> }/>
+          {/*<Route path="/signout" render={ () => <UserSignOut /> }/>*/}
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div class="header">
-            <div class="bounds">
-                <h1 class="header--logo">Courses</h1>
+        <div className="header">
+            <div className="bounds">
+                <h1 className="header--logo">Courses</h1>
                 <nav>
-                    <a class="signup" href="sign-up.html">Sign Up</a>
-                    <a class="signin" href="sign-in.html">Sign In</a>
+                    <a className="signup" href="sign-up.html">Sign Up</a>
+                    <a className="signin" href="sign-in.html">Sign In</a>
                 </nav>
             </div>
         </div>
