260166b277990f9f8b8f6d9bb1967227bbcd913c
Rodolfo Perez <rodolfobeste@gmail.com>
2019-02-13 02:27:18 +0200

    Added error handler for 404 and 500 status, removed unused consumer context

--- client/src/components/courses/Courses.js
+++ client/src/components/courses/Courses.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import SingleCourse from "./SingleCourse";
-import { Link } from "react-router-dom"; 
-import { Consumer } from "../Context/index";
+import { Link, withRouter } from "react-router-dom";
 import axios from "axios";
 
 // Root route which displays all the available courses
@@ -9,7 +8,8 @@ import axios from "axios";
 class Courses extends Component {
 
     state = {
-        courses: []
+        courses: [],
+        errors: []
     }
 
     fetchCourses = () => {
@@ -22,7 +22,12 @@ class Courses extends Component {
             data.forEach( course => coursestitle.push({ title: course.title, id: course._id }));
             return this.setState({courses: coursestitle});
         })
-        .catch(error => console.log("Error fetching and parsing data", error));
+        .catch(error => { if( error.response.status === 404) {
+            this.props.history.push("/notfound");
+        } else {
+            this.props.history.push("/error");
+            }
+          } );
     }
 
     // Gets the collection of all courses in DB
@@ -41,24 +46,19 @@ class Courses extends Component {
 
     render () {
         return(
-            <Consumer>
-                { context => (
-                        
-                    context.isLoading ? (<div>Loading {this.fetchCourses()} {/* updates courses state by getting all courses from DB while "loading*/}</div>)
-                    : ( <div className="bounds">
-                            { this.displayCourses()}
-                            <div className="grid-33">
-                                <Link to="/courses/create" className="course--module course--add--module">
-                                    <h3 className="course--add--title"> <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>New Course</h3>
-                                </Link>
-                            </div>
-                        </div>
-                    ) 
 
-                )}
-            </Consumer>
+            <div className="bounds">
+                { this.displayCourses()}
+                <div className="grid-33">
+                    <Link to="/courses/create" className="course--module course--add--module">
+                        <h3 className="course--add--title"> <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>New Course</h3>
+                    </Link>
+                </div>
+            </div>
+
         )
     }
+
 }
 
-export default Courses;
+export default withRouter(Courses);
